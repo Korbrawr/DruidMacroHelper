c803e9ffa596936f70129195b701a383bcb4ebed
Korbrawr <iamkorbah@gmail.com>
2024-05-12 13:51:07 -0700

    updated ToC for 40400

--- DruidMacroHelper.lua
+++ DruidMacroHelper.lua
@@ -182,8 +182,10 @@ function DruidMacroHelper:OnSlashSnake(parameters)
       self:LogDebug("LibClassicSwingTimerAPI not installed, snaking naively");
       self:SnakeHelper(parameters);
     else
-      local time_till_next_auto = select(2, LibClassicSwingTimerAPI:SwingTimerInfo("mainhand")) - GetTime();
-      if time_till_next_auto > (1 / (1 + GetMeleeHaste() / 100)) then
+      local catSwingTimer = 1 / (1 + GetMeleeHaste()/100);
+      --local time_till_next_auto = select(2, LibClassicSwingTimerAPI:SwingTimerInfo("mainhand")) - GetTime();
+      --if time_till_next_auto > (1 / (1 + GetMeleeHaste() / 100)) then
+      if GetTime() + catSwingTimer then
         self:LogDebug("Summoning snake to clip next swing down from", time_till_next_auto);
         self:SnakeHelper(parameters);
       else
